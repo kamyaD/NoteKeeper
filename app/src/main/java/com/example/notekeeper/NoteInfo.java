83151cd6db4292c3e95c071f6d4b088b601d4e77
Kamya Domnick <kamya.domnick@andela.com>
2019-12-21 09:17:13 +0300

    initial commit

---
+++ app/src/main/java/com/example/notekeeper/NoteInfo.java
@@ -0,0 +1,115 @@
+package com.example.notekeeper;
+
+import android.annotation.SuppressLint;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+@SuppressLint("ParcelCreator")
+public final class NoteInfo implements Parcelable {
+    private CourseInfo mCourse;
+    private String mTitle;
+    private String mText;
+
+    public NoteInfo(CourseInfo course, String title, String text) {
+        mCourse = course;
+        mTitle = title;
+        mText = text;
+    }
+
+    private NoteInfo(Parcel parcel) {
+
+        mCourse = parcel.readParcelable(CourseInfo.class.getClassLoader());
+        mTitle = parcel.readString();
+        mText = parcel.readString();
+    }
+
+    public static final Creator<NoteInfo> CREATOR = new Creator<NoteInfo>() {
+        @Override
+        public NoteInfo createFromParcel(Parcel in) {
+            return new NoteInfo(in);
+        }
+
+        @Override
+        public NoteInfo[] newArray(int size) {
+            return new NoteInfo[size];
+        }
+    };
+
+    public CourseInfo getCourse() {
+        return mCourse;
+    }
+
+    public void setCourse(CourseInfo course) {
+        mCourse = course;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    private String getCompareKey() {
+        return mCourse.getCourseId() + "|" + mTitle + "|" + mText;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NoteInfo that = (NoteInfo) o;
+
+        return getCompareKey().equals(that.getCompareKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return getCompareKey().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getCompareKey();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel parcel, int i) {
+        parcel.writeParcelable(mCourse, 0);
+        parcel.writeString(mTitle);
+        parcel.writeString(mText);
+
+        final Creator<NoteInfo> CREATOR = new Creator<NoteInfo>() {
+
+            private Parcel parcel;
+
+            @Override
+            public NoteInfo createFromParcel(Parcel parcel) {
+                this.parcel = parcel;
+                return new NoteInfo(parcel);
+            }
+
+            @Override
+            public NoteInfo[] newArray(int size) {
+                return new NoteInfo[size];
+            }
+        };
+
+
+    }
+}
